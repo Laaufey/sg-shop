8de70ae247544a6274e010a4cf81b6dbb8b9ee8f
Laufey <laufey@gmail.com>
2021-05-25 18:48:16 +0200

    sanity

--- eyanashop/schemas/productVariant.js
+++ eyanashop/schemas/productVariant.js
@@ -1,50 +1,45 @@
 export default {
-  title: 'Product variant',
-  name: 'productVariant',
-  type: 'object',
+  title: "Product variant",
+  name: "productVariant",
+  type: "object",
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Weight in grams',
-      name: 'grams',
-      type: 'number',
+      title: "Weight in grams",
+      name: "grams",
+      type: "number",
     },
     {
-      title: 'Price',
-      name: 'price',
-      type: 'number',
+      title: "Price",
+      name: "price",
+      type: "number",
     },
     {
-      title: 'SKU',
-      name: 'sku',
-      type: 'string',
+      title: "SKU",
+      name: "sku",
+      type: "string",
     },
     {
-      title: 'Taxable',
-      name: 'taxable',
-      type: 'boolean',
+      title: "Taxable",
+      name: "taxable",
+      type: "boolean",
     },
     {
-      name: 'images',
-      title: 'Images',
-      type: 'array',
-      of: [
-        {
-          type: 'image',
-          options: {
-            hotspot: true,
-          },
-        },
-      ],
+      name: "images",
+      title: "Images",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
     },
     {
-      title: 'Bar code',
-      name: 'barcode',
-      type: 'barcode',
+      title: "Bar code",
+      name: "barcode",
+      type: "barcode",
     },
   ],
-}
+};
--- src/pages/Fragrances.js
+++ src/pages/Fragrances.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client";
 
 export default function Fragrances() {
-  return <h1>Fragrances Page!</h1>;
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "product"]{
+          title,
+          slug,
+          categories,
+          defaultProductVariant{
+            "imageUrl": images.asset->url,
+            price
+          }
+        }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <h1>Fragrances Page!</h1>
+        <h2>Products</h2>
+        <div>
+          {postData &&
+            postData.map((product, index) => (
+              <article>
+                {console.log(product.defaultProductVariant)}
+                <Link
+                  to={"/product/" + product.slug.current}
+                  key={product.slug.current}
+                >
+                  <span key={index}>
+                    <img
+                      src={product.defaultProductVariant.imageUrl}
+                      alt={product.title}
+                    />
+                    <span>
+                      <h3>{product.title}</h3>
+                      <h3></h3>
+                    </span>
+                  </span>
+                </Link>
+
+                <h4>price: {product.defaultProductVariant.price} </h4>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
 }
